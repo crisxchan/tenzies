5944162dc1823ac3c68e655e708fdc0c58ab8595
crisxchan <chrisgonzales.online@gmail.com>
2022-08-07 18:22:21 +0800

    Create basic structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <div className="tenzies-wrapper">
+        <h1>Tenzies</h1>
+        <p>Roll until all dice are the same. Click 
+          each die to freeze it at its current value
+          between rolls.
+        </p>
+      </div>
     </div>
   );
 }
